63d70e7d2265e3749a24b2ac993d4777001a6a16
UppercutsFromKupchino <78866503+UppercutsFromKupchino@users.noreply.github.com>
2026-04-18 06:52:28 +0300

    add files

---
+++ app/ui/fonts.ts
@@ -0,0 +1,17 @@
+import { Inter } from 'next/font/google';
+import { Saira_Stencil_One } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+    subsets: ['latin']
+});
+
+export const saira = Saira_Stencil_One({
+    weight: "400",
+    subsets: ["latin"],
+});
+
+export const lusitana = Lusitana({
+    weight: '400',
+    subsets: ['latin']}
+);
